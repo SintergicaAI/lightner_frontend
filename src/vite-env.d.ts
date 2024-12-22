4835d8261a0fe3cbd060a3ebe5e368c042e47d51
Vegg1eC0de <alexis.alvarez@sintergica.ai>
2024-12-22 12:40:35 -0600

    Init repository configs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_DB_HOST: string;
+    readonly VITE_DB_USER: string;
+    readonly VITE_DB_PASSWORD: string;
+    readonly VITE_DB_NAME: string;
+    readonly VITE_DB_PORT: string;
+    readonly VITE_API_HOST: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
