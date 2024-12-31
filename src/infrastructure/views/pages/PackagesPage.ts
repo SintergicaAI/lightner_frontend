93d094cd87b0596fdbc0391bbd453bfc97583b37
Vegg1eC0de <alexis.alvarez@sintergica.ai>
2024-12-30 20:02:21 -0600

    Add dynamic calendar component to main layout
    Introduced a dynamic calendar with day selection functionality. Added `initializeCalendar`, `Calendar`, and `CalendarDay` components to handle rendering and interactions. Updated the main layout to include the calendar container and display the selected date.

--- src/infrastructure/views/pages/PackagesPage.ts
+++ src/infrastructure/views/pages/PackagesPage.ts
@@ -1,14 +1,31 @@
-import servicesTemplate from "../templates/service.html?raw"; // Import the template as text without processing
+import packageTemplate from "../templates/service.html?raw"; // Import the template as text without processing
 import {setupCounter} from "../components/counter.ts";
 import {initializeServicesForm} from "../../../services.form.ts";
 import {ping} from "../../../adapters/api/ping.ts";
+import {Calendar} from "../components/Calendar.ts";
 
+
+const initializeCalendar = () => {
+    const app = document.querySelector('#app');
+
+    if (!app) {
+        throw new Error("No se encontró el contenedor '#app'");
+    }
+
+    // Configuración del calendario
+    const daysInMonth = 30; // Ejemplo: Octubre tiene 30 días
+    const currentMonth = '10'; // Octubre
+    const currentYear = '2023';
+
+    const calendar = Calendar(daysInMonth, currentMonth, currentYear);
+    app.appendChild(calendar);
+}
 
 export function PackagesPage(): void {
 
     const app = document.getElementById("lightner-content");
     if (!app) throw new Error("Elemento 'app' no encontrado")
-    app.innerHTML = servicesTemplate;
+    app.innerHTML = packageTemplate;
     setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
 
@@ -21,4 +38,6 @@ export function PackagesPage(): void {
     }
 
     initializeServicesForm(document.querySelector<HTMLFormElement>('#form')!)
+    document.addEventListener('DOMContentLoaded', initializeCalendar);
+
 }
